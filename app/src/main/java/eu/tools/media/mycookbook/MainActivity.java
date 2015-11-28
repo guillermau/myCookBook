7b2c89db4f4463eff241e5625b68e7d96d54d088
guillermau <bourdat.guillaume@gmail.com>
2015-11-28 15:56:33 +0100

    see #1.3 : verification test and feedback to the user

--- app/src/main/java/eu/tools/media/mycookbook/MainActivity.java
+++ app/src/main/java/eu/tools/media/mycookbook/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText m_login = null;
     private EditText m_password = null;
+    private EditText m_errorMessage= null;
     private Button m_signInButton = null;
     private Button m_passwordButton = null;
 
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         m_password = (EditText) findViewById(R.id.password);
         m_signInButton = (Button) findViewById(R.id.buttonLogin);
         m_passwordButton = (Button) findViewById(R.id.buttonPassword);
+        m_errorMessage = (EditText) findViewById(R.id.errorMessage);
 
         // Sznd saved name on interface
         SharedPreferences profile = getSharedPreferences(PREFS_NAME, 0);
@@ -85,11 +87,29 @@ public class MainActivity extends AppCompatActivity {
                                     JSONArray rows = response.getJSONArray("rows");
                                     JSONObject value = rows.getJSONObject(0).getJSONObject("value");
 
-                                    if (value.getString("password") == m_password.getText().toString()) {
+                                    if (value.getString("password").equals(m_password.getText().toString())) {
                                         // good password
+                                        m_errorMessage.setVisibility(View.INVISIBLE);
+
+                                        // We need an Editor object to make preference changes.
+                                        // All objects are from android.context.Context
+                                        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                                        SharedPreferences.Editor editor = settings.edit();
+                                        editor.putString ("username", m_login.getText().toString());
+                                        editor.putString ("password", m_password.getText().toString());
+
+                                        editor.commit(); // Commit the edits!
+
+
+
+                                        // new activity: Your cook book
+                                        Intent intent = new Intent(MainActivity.this, activityProfileTest.class);
+                                        // intent.putExtra(EXTRA_LOGIN, m_login.getText().toString());
+                                        startActivity(intent);
                                     }
                                     else {
                                         // bad password
+                                        m_errorMessage.setVisibility(View.VISIBLE);
                                     }
 
                                 } catch (JSONException exp) {
@@ -104,23 +124,6 @@ public class MainActivity extends AppCompatActivity {
                 });
                 // Add the request to the RequestQueue.
                 queue.add(passwordRequest);
-
-
-                // We need an Editor object to make preference changes.
-                // All objects are from android.context.Context
-                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-                SharedPreferences.Editor editor = settings.edit();
-                editor.putString ("username", m_login.getText().toString());
-                editor.putString ("password", m_password.getText().toString());
-
-                editor.commit(); // Commit the edits!
-
-
-
-                // new activity: Your cook book
-                Intent intent = new Intent(MainActivity.this, activityProfileTest.class);
-                // intent.putExtra(EXTRA_LOGIN, m_login.getText().toString());
-                startActivity(intent);
 
             }
         });
