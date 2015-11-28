a46e93243858e0681b5ab4b34e8a69fdd0fb12b7
guillermau <bourdat.guillaume@gmail.com>
2015-11-28 15:13:01 +0100

    see #0 : import database information in preferences

--- app/src/main/java/eu/tools/media/mycookbook/MainActivity.java
+++ app/src/main/java/eu/tools/media/mycookbook/MainActivity.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     public static final String PREFS_NAME = "SavedLogin";
+    public static final String PREFS_PROFILE = "SavedProfile";
+    public static final String PREFS_DATABASE = "SavedDatabase";
 
     private EditText m_login = null;
     private EditText m_password = null;
@@ -21,12 +23,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set database prefs
+        SharedPreferences settings = getSharedPreferences(PREFS_DATABASE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("hostname", "couchdb.bourdat.eu");
+        editor.putString("database", "mycookbook");
+        editor.putString("port", "5984");
+        editor.putString("user", "");
+        editor.putString("password","");
+        editor.putString("url", "http://couchdb.bourdat.eu:5984/mycookbook/");
+
+        editor.commit(); // Commit the edits!
+
         // get login and password
         m_login = (EditText) findViewById(R.id.username);
         m_password = (EditText) findViewById(R.id.password);
         m_signInButton = (Button) findViewById(R.id.buttonLogin);
         m_passwordButton = (Button) findViewById(R.id.buttonPassword);
 
+        // Sznd saved name on interface
         SharedPreferences profile = getSharedPreferences(PREFS_NAME, 0);
         String savedUsername = profile.getString("username", "false");
         if (savedUsername!= "false") {
@@ -36,9 +52,6 @@ public class MainActivity extends AppCompatActivity {
         if (savedUsername!= "false") {
             m_password.setText(savedPassword);
         }
-
-        final String EXTRA_LOGIN = "user_login";
-        final String EXTRA_PASSWORD = "user_password";
 
         m_signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,13 +64,13 @@ public class MainActivity extends AppCompatActivity {
                 editor.putString ("username", m_login.getText().toString());
                 editor.putString ("password", m_password.getText().toString());
 
-                // Commit the edits!
-                editor.commit();
+                editor.commit(); // Commit the edits!
+
+
 
                 // new activity: Your cook book
                 Intent intent = new Intent(MainActivity.this, activityProfileTest.class);
-                intent.putExtra(EXTRA_LOGIN, m_login.getText().toString());
-                intent.putExtra(EXTRA_PASSWORD, m_password.getText().toString());
+                // intent.putExtra(EXTRA_LOGIN, m_login.getText().toString());
                 startActivity(intent);
 
             }
