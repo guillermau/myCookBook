5ceb77d3e47fc0fd364d427d55aec8af532c4043
guillermau <bourdat.guillaume@gmail.com>
2015-12-01 00:19:38 +0100

    Implementation of network getting

--- app/src/main/java/eu/tools/media/mycookbook/activityResearch.java
+++ app/src/main/java/eu/tools/media/mycookbook/activityResearch.java
@@ -1,6 +1,7 @@
 package eu.tools.media.mycookbook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,6 +10,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -20,10 +32,11 @@ import eu.tools.media.mycookbook.model.Recipe;
 import eu.tools.media.mycookbook.model.UsedIngredient;
 import eu.tools.media.mycookbook.model.User;
 
-/**
- * Created by benedictefahrer on 19/11/2015.
- */
 public class activityResearch extends AppCompatActivity {
+
+    static final String PREFS_NAME = "SavedLogin";
+    static final String PREFS_PROFILE = "SavedProfile";
+    static final String PREFS_DATABASE = "SavedDatabase";
 
     final String EXTRA_LOGIN = "user_login";
     final String EXTRA_PASSWORD = "user_password";
@@ -36,6 +49,41 @@ public class activityResearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_research);
         Intent intent = getIntent();
+
+        SharedPreferences profile = getSharedPreferences(PREFS_DATABASE, 0);
+        String baseUrl = profile.getString("url", "false");
+        String url = baseUrl+"_design/allergen/_view/byName";
+
+        // Load queue for connection
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest passwordRequest = new JsonObjectRequest(Request.Method.GET, url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray rows = response.getJSONArray("rows");
+
+                            for (int i = 0; i < rows.length() ; i++) {
+                                JSONObject allergen = rows.getJSONObject(i);
+                                String allergenName = allergen.getString("key");
+                                String allergenId = allergen.getString("id");
+                            }
+
+                        } catch (JSONException exp) {
+                            Log.e("Error", "Bad JSON", exp);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO handle error
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(passwordRequest);
+
 
 
         // Création d'un user
@@ -84,7 +132,13 @@ public class activityResearch extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
+                // Send a list of allergen
 
+                // Bind ingredient
+
+                // Get recipes
+
+                // Send recipes id to the new activity
             }
         });
 
