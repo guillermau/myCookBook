5e5f02c5a56614c606877b0fc70743f50ce324b4
guillermau <bourdat.guillaume@gmail.com>
2015-12-01 17:05:04 +0100

    Implementation of update functions

--- app/src/main/java/eu/tools/media/mycookbook/activityEditRecipe.java
+++ app/src/main/java/eu/tools/media/mycookbook/activityEditRecipe.java
@@ -1,6 +1,7 @@
 package eu.tools.media.mycookbook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import java.util.HashMap;
 
 import eu.tools.media.mycookbook.model.Connection;
 import eu.tools.media.mycookbook.model.Ingredient;
@@ -19,44 +34,121 @@ import eu.tools.media.mycookbook.model.UsedIngredient;
 import eu.tools.media.mycookbook.model.User;
 
 public class activityEditRecipe extends AppCompatActivity  {
+
+    static final String PREFS_NAME = "SavedLogin";
+    static final String PREFS_PROFILE = "SavedProfile";
+    static final String PREFS_DATABASE = "SavedDatabase";
+
+    final String EXTRA_RECIPEID = "idRecipe";
+
     EditText m_editText = null;
     FloatingActionButton m_saveButton;
-    final String EXTRA_LOGIN = "user_login";
-    final String EXTRA_PASSWORD = "user_password";
-    final String EXTRA_POSITION = "position";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editrecipe);
         Intent intent = getIntent();
 
+        final String IdRecipe = intent.getStringExtra(EXTRA_RECIPEID);
+
+        SharedPreferences profile = getSharedPreferences(PREFS_DATABASE, 0);
+        final String baseUrl = profile.getString("url", "false");
+
+        // Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String urlRecipe = baseUrl+IdRecipe;
+
         m_editText = (EditText) findViewById(R.id.editText);
         m_saveButton = (FloatingActionButton) findViewById(R.id.fab);
 
-        // Création d'un user
-        Connection connection = new Connection("couchDB.Boudrat.eu","5984","mycookbook");
-        final User myUser = new User(connection,intent.getStringExtra(EXTRA_LOGIN),intent.getStringExtra(EXTRA_PASSWORD));
+        JsonObjectRequest recipeRequest = new JsonObjectRequest(Request.Method.GET, urlRecipe,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject recipe) {
+                        try {
+                            m_editText.setText(recipe.getString("instructions"), EditText.BufferType.EDITABLE);
+                            Log.i("instructions", recipe.getString("instructions"));
+                        } catch (JSONException exp) {
+                            Log.e("Error","Bad JSON", exp);
+                        }
 
-        // get the list of recipe
-        final ArrayList<Recipe> listRecette = myUser.getCookbook();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error","Bad JSON", error);
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(recipeRequest);
 
-        // get the recipe we want ti visualize
-        final int position = intent.getIntExtra(EXTRA_POSITION, 0);
-        Log.d("debug", "position" + position);
-
-        final Recipe recette = listRecette.get(position);
-        String instructions = "\n";
-        instructions += recette.getInstructions();
-        m_editText.setText(instructions,EditText.BufferType.EDITABLE);
 
         m_saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // save the text
-                recette.setInstructions(m_editText.getText().toString());
-                listRecette.set(position, recette);
-                Log.d("debug", recette.getInstructions());
+                JsonObjectRequest recipeRequest = new JsonObjectRequest(Request.Method.GET, urlRecipe,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject recipe) {
 
+                                final JSONObject transRecipe = recipe;
+
+                                JsonObjectRequest udapteRecipeRequest = new JsonObjectRequest(Request.Method.PUT, urlRecipe,
+                                        new Response.Listener<JSONObject>() {
+                                            @Override
+                                            public void onResponse(JSONObject responseRequest) {
+                                                // Launching new Activity on selecting single List Item
+                                                Intent i = new Intent(getApplicationContext(), activityVisuRecipe.class);
+                                                // sending data to new activity
+                                                i.putExtra("idRecipe", IdRecipe);
+                                                startActivity(i);
+
+                                            }
+                                        }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        // TODO Handle error
+                                    }
+                                }) {
+
+                                    @Override
+                                    public HashMap<String, String> getParams() {
+                                        HashMap<String, String> params = new HashMap<String, String>();
+                                        params.put("Content-Type", "application/json; charset=utf-8");
+
+                                        return params;
+                                    }
+
+                                    // Returns the raw POST or PUT body to be sent.
+                                    @Override
+                                    public byte[] getBody() {
+                                        try {
+                                            JSONObject toSend = transRecipe;
+                                            toSend.put("instructions", m_editText.getText().toString());                                            ;
+                                            return toSend.toString().getBytes("UTF-8");
+                                        }
+                                        catch (UnsupportedEncodingException exep) {
+                                            return null;
+                                        }
+                                        catch (JSONException exep) {
+                                            return null;
+                                        }
+                                    }
+
+                                };
+                                // Add the request to the RequestQueue.
+                                queue.add(udapteRecipeRequest);
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("EditRecipe", "Save Error" , error);
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(recipeRequest);
 
             }
         });
